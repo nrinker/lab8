b50089cbcf44bb672f2d800d0d0b8aed42b93171
nrinker <nrinker@ucsd.edu>
2021-02-25 10:02:07 -0800

    Finsihed Lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'd2DC97KuPej3UGPi6frh4U0R6xOTNHaY';
+	var map = L.mapquest.map('map', {
+		center: [32.878736528454965, -117.23587527292489],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+	L.marker([32.878736528454965, -117.23587527292489]).addTo(map);
 }
